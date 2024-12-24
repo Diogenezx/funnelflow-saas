2d70f75ab03b7e1bad2c4b92a881691c51710e8f
Diógenes Menezes <Diogenezx@gmail.com>
2024-12-24 00:04:08 -0300

    feat: Add root layout component

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Providers } from './providers'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'FunnelFlow | Marketing e Vendas Inteligentes',
+  description: 'Crie funis de vendas dinâmicos e interativos com facilidade',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
